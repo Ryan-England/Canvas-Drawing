9503868047157f4912c9be704c34cc96dbfabc0f
Ryan England <raenglandwork@gmail.com>
2024-10-22 15:03:43 -0700

    Added Canvas and canvas stylization. Step 1 complete.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import "./style.css";
 
-const APP_NAME = "Hello, welcome to our Canvas.";
+const APP_NAME = "Hello, welcome to our Canvas";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+
+const header = document.createElement("h1");
+header.innerHTML = "Please draw here:";
+app.append(header);
+
+/*
+const canvasGet = document.getElementById("canvas");
+const ctx = canvasGet.getContext("2d");
+*/
+const canvas = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
+app.append(canvas);
