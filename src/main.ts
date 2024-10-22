7e65cf35e13f2ef0d171e73e3f3abcc0a2929a3d
Ryan England <raenglandwork@gmail.com>
2024-10-22 16:13:54 -0700

    Added drawing functionality and a clear button. Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,62 @@ header.innerHTML = "Please draw here:";
 app.append(header);
 
 const canvas = document.createElement("canvas");
+canvas.id = "getCanvas"
 canvas.height = 256;
 canvas.width = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+let isDrawing: boolean = false;
+let x: number = 0;
+let y: number = 0;
+
+canvas.addEventListener("mousedown", (m) => {
+  x = m.offsetX;
+  y = m.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (m) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, m.offsetX, m.offsetY);
+    x = m.offsetX;
+    y = m.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (m) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, m.offsetX, m.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function clear() {
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx?.clearRect(0, 0, 256, 256);
+  }
+}
+
+const clearButton = document.createElement("button")
+clearButton.textContent = "Clear";
+clearButton.addEventListener("click", () => {
+  clear();
+});
+app.append(clearButton);
+
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
